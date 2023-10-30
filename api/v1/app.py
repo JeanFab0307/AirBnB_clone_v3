c6d23f09b878929cb0cb2a33f3661dae5dcbbc05
JeanFab0307 <jfrandriatsimivony@gamil.com>
2023-10-30 16:56:43 +0300

    Add packages to run first RESTFUL api

---
+++ api/v1/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" module doc"""
+
+
+from flask import Flask
+from os import getenv
+from models import storage
+from api.v1.views import app_views
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix='/api/v1')
+
+@app.teardown_appcontext
+def close(ctx):
+    storage.close()
+
+if __name__ == '__main__':
+    HBNB_API_HOST = getenv('HBNB_API_HOST')\
+        if getenv('HBNB_API_HOST') is not None else '0,0,0,0' 
+    HBNB_API_PORT = getenv('HBNB_API_PORT')\
+        if getenv('HBNB_API_PORT') is not None else '5000'
+    
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
---
+++ api/v1/views/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Initialize blueprint"""
+
+
+from flask import Blueprint
+
+
+app_views = Blueprint('app_views', __name__)
+
+
+from api.v1.views.index import *
